a92c97f2f90fc8becb6fd27a4189702ee9a3f270
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2020-03-29 12:40:05 -0400

    Restyled cart view and added home view

--- src/app/modules/users/components/favorites/favorites.component.ts
+++ src/app/modules/users/components/favorites/favorites.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
+  verse: string;
+  imgUrl: string;
+
   constructor() { }
 
   ngOnInit() {
+    fetch('https://developers.youversionapi.com/1.0/verse_of_the_day/12?version_id=206', {
+      headers: {
+        'X-YouVersion-Developer-Token': 'lhGSP91Ig3J_1kLTC6FEm5I6HtE',
+        'Accept-Language': 'en',
+        Accept: 'application/json',
+      }
+    })
+      .then((result) => result.json())
+      .then((json) => {
+        var verseObject = json['verse'];
+        console.log(json);
+        this.verse = json.verse.text;
+        this.imgUrl = json.image.url;
+      });
+
+
+    fetch('https://developers.youversionapi.com/1.0/versions', {
+      headers: {
+        'X-YouVersion-Developer-Token': 'lhGSP91Ig3J_1kLTC6FEm5I6HtE',
+        'Accept-Language': 'en',
+        Accept: 'application/json',
+      }
+    })
+      .then((result) => result.json())
+      .then((json) => console.log(json))
+
+
+  }
+
+  fetchVerseForDay(day: number) {
+
+  }
+
+  clicked() {
+    console.log("CLICKED!");
   }
 
 }
