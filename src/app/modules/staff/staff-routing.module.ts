2ca32b3ae5d9cfb170058a83a895baa296f8a2f8
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2020-03-31 18:42:54 -0400

    Demo gif added

--- src/app/modules/staff/staff-routing.module.ts
+++ src/app/modules/staff/staff-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule } from '@angular/router';
 import { StaffComponent } from './staff/staff.component';
 import { IncomingOrdersComponent } from './components/incoming-orders/incoming-orders.component';
 import { HoursComponent } from './components/cafe/hours/hours.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 const routes = [
     { path: '', component: StaffComponent },
-    { path: 'cafe/hours', component: HoursComponent }
+    { path: 'cafe/hours', component: HoursComponent },
+    { path: 'cafe', component: SettingsComponent }
 ];
 
 @NgModule({
