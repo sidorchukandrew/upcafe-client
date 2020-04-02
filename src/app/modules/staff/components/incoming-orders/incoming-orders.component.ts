5ce084abac5b826299d0408aa632e3aa9afafbc3
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2020-04-02 01:00:29 -0400

    Restyled to meet client's requests

--- src/app/modules/staff/components/incoming-orders/incoming-orders.component.ts
+++ src/app/modules/staff/components/incoming-orders/incoming-orders.component.ts
@@ -11,10 +11,12 @@ import { VariationData } from 'src/app/models/VariationData';
 export class IncomingOrdersComponent implements OnInit {
 
   selectedOrdersView: string;
-
+  showOptions: boolean;
   orders: Array<Order>;
 
-  constructor() { }
+  constructor() {
+    this.showOptions = false;
+  }
 
   ngOnInit() {
     this.selectedOrdersView = "Up Next";
@@ -69,21 +71,69 @@ export class IncomingOrdersComponent implements OnInit {
 
     var order2: Order = {
       id: 'PLaqn819Nak1o',
-      pickupTime: '11:10',
+      pickupTime: '10:10',
+      totalPrice: 1.5,
+      selectedLineItems: lineItems2
+    }
+    var order3: Order = {
+      id: 'PLaqn819Nak1o',
+      pickupTime: '13:10',
+      totalPrice: 1.5,
+      selectedLineItems: lineItems2
+    }
+    var order4: Order = {
+      id: 'PLaqn819Nak1o',
+      pickupTime: '9:10',
       totalPrice: 1.5,
       selectedLineItems: lineItems2
     }
 
     this.orders = new Array<Order>();
 
-    this.orders.push(order);
     this.orders.push(order2);
+    this.orders.push(order);
+    this.orders.push(order3);
+    this.orders.push(order4);
+
+    this.orders.sort((a, b) => {
+
+      var hourA: number = this.parseHour(a.pickupTime);
+      var hourB: number = this.parseHour(b.pickupTime);
+
+      var minutesA: number = this.parseMinutes(a.pickupTime);
+      var minutesB: number = this.parseMinutes(b.pickupTime);
+
+      if (hourA != hourB)
+        return hourA - hourB;
+
+      else
+        return minutesA - minutesB;
+
+    });
   }
 
-  async selectOrderView(view: string) {
+  parseHour(time: string): number {
+    var indexOfColon = time.indexOf(":");
+    return parseInt(time.slice(0, indexOfColon));
+  }
 
-    console.log(this.orders);
-    // await this.delay(200);
+  parseMinutes(time: string): number {
+    var indexOfColon = time.indexOf(":");
+    return parseInt(time.slice(indexOfColon + 1, time.length));
+  }
+
+  convertTime(time: string): string {
+
+    var indexOfColon = time.indexOf(":");
+    var hour = parseInt(time.slice(0, indexOfColon));
+
+    if (hour > 12)
+      hour -= 12;
+
+    return (hour + ":" + time.slice(indexOfColon + 1, time.length));
+  }
+
+  selectOrderView(view: string): void {
     this.selectedOrdersView = view;
   }
 
