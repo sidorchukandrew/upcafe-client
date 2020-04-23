6be69e2d1234734558dd08c154fd6ddf7a7901c7
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2020-04-23 00:07:16 -0400

    Prevent customers from adding items to their order when an order has already been placed

--- src/app/modules/users/components/menu/item-details/item-details.component.ts
+++ src/app/modules/users/components/menu/item-details/item-details.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit, Input, ViewChild, OnDestroy } from '@angular/core';
-import { LineItem } from 'src/app/models/LineItem';
-import { ModifierListData } from 'src/app/models/ModifierListData';
+import { Component, OnInit, Input, ViewChild, OnDestroy } from "@angular/core";
+import { LineItem } from "src/app/models/LineItem";
+import { ModifierListData } from "src/app/models/ModifierListData";
 import { ActivatedRoute, Router } from "@angular/router";
-import { ModListDetailsComponent } from '../mod-list-details/mod-list-details.component';
-import { OrderService } from 'src/app/services/order.service';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { SelectedItemStore } from 'src/app/services/stores/selected-item.store';
-import { Subscription, noop } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { VariationData } from 'src/app/models/VariationData';
-
+import { ModListDetailsComponent } from "../mod-list-details/mod-list-details.component";
+import { OrderService } from "src/app/services/order.service";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { SelectedItemStore } from "src/app/services/stores/selected-item.store";
+import { Subscription, noop, Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { VariationData } from "src/app/models/VariationData";
 
 @Component({
-  selector: 'app-item-details',
-  templateUrl: './item-details.component.html',
-  styleUrls: ['./item-details.component.css']
+  selector: "app-item-details",
+  templateUrl: "./item-details.component.html",
+  styleUrls: ["./item-details.component.css"],
 })
 export class ItemDetailsComponent implements OnInit, OnDestroy {
-
   @ViewChild(ModListDetailsComponent, { static: false })
   private modListDetailsComponent: ModListDetailsComponent;
 
@@ -28,20 +26,30 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
   nameOfCurrentlySelectedModifierList: string;
   subscriptions: Subscription;
   item: LineItem;
+  orderState$: Observable<string>;
 
-  constructor(private itemStore: SelectedItemStore, private route: ActivatedRoute,
-    private orderService: OrderService, public userResponseDialog: MatDialog, private router: Router) {
-
-  }
+  constructor(
+    private itemStore: SelectedItemStore,
+    private orderService: OrderService,
+    public userResponseDialog: MatDialog,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.orderState$ = this.orderService.state$;
     this.subscriptions = new Subscription();
 
-    this.subscriptions.add(this.itemStore.currentItem$.pipe(
-      tap(item => (item) ? noop : this.router.navigate(["user/menu/"])),
-      tap(item => this.parsePrice(item.variationData.variationPrice)),
-      tap(item => this.item = item)
-    ).subscribe());
+    this.subscriptions.add(
+      this.itemStore.currentItem$
+        .pipe(
+          tap((item) => (item ? noop : this.router.navigate(["user/menu/"]))),
+          tap((item) => this.parsePrice(item.variationData.variationPrice)),
+          tap((item) => (this.item = item))
+        )
+        .subscribe()
+    );
+
+    this.subscriptions.add(this.orderState$.subscribe());
   }
 
   ngOnDestroy() {
@@ -49,20 +57,20 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
   }
 
   parsePrice(price: number): void {
-    var index: number = price.toString().indexOf('.');
+    var index: number = price.toString().indexOf(".");
 
     if (index != -1) {
       this.priceDollars = parseInt(price.toString().substr(0, index));
-      this.priceCents = parseInt(price.toString().substr(index + 1, price.toString().length));
-    }
-    else {
+      this.priceCents = parseInt(
+        price.toString().substr(index + 1, price.toString().length)
+      );
+    } else {
       this.priceDollars = price;
-      this.priceCents = '00';
+      this.priceCents = "00";
     }
   }
 
   async counter(start: number, end: number, durationMs: number) {
-
     var delayTime: number = durationMs / (end - start);
 
     while (start < end) {
@@ -73,7 +81,7 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
   }
 
   private delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   public loadModifierList(modifierListData: ModifierListData): void {
@@ -81,33 +89,36 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
   }
 
   public addToOrder(): void {
-
     var selectedModifiers;
     if (this.modListDetailsComponent)
       selectedModifiers = this.modListDetailsComponent.getSelectedModifiers();
 
     var variationData: VariationData = this.item.variationData;
-    if (variationData.name == 'Regular')
+    if (variationData.name == "Regular")
       variationData.name = this.item.itemData.name;
 
-    var orderItem = this.orderService.newOrderItem(variationData, selectedModifiers);
+    var orderItem = this.orderService.newOrderItem(
+      variationData,
+      selectedModifiers
+    );
     this.orderService.addToOrder(orderItem);
 
     this.userResponseDialog.open(UserResponseDialog, {
-      hasBackdrop: true
+      hasBackdrop: true,
     });
   }
 }
 
 @Component({
-  selector: 'user-response-dialog',
-  templateUrl: 'user-response-dialog.html',
-  styleUrls: ['user-response-dialog.css']
+  selector: "user-response-dialog",
+  templateUrl: "user-response-dialog.html",
+  styleUrls: ["user-response-dialog.css"],
 })
 export class UserResponseDialog implements OnInit {
-
   constructor(
-    public dialogRef: MatDialogRef<UserResponseDialog>, private router: Router) { }
+    public dialogRef: MatDialogRef<UserResponseDialog>,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     window.navigator.vibrate(200);
@@ -119,7 +130,7 @@ export class UserResponseDialog implements OnInit {
   }
 
   viewOrder(): void {
-    this.router.navigate(['user/cart']);
+    this.router.navigate(["user/cart"]);
     this.close();
   }
 }
--- src/app/modules/users/user/user.component.ts
+++ src/app/modules/users/user/user.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { OrderService } from "src/app/services/order.service";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  constructor(private ordersService: OrderService) {}
 
   ngOnInit() {
-
+    this.ordersService.checkIfOrderAlreadyPlaced().subscribe();
   }
-
 }
