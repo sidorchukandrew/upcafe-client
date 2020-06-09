6ea43bff212dc7396114e3bc22d398c31a29bd25
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2020-06-08 23:08:17 -0400

    Styled category filter buttons and added search bar

--- src/app/modules/users/components/menu/menu.component.ts
+++ src/app/modules/users/components/menu/menu.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ThemeService } from 'src/app/services/theme.service';
+import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  protected darkThemeOn: boolean = false;
+  protected category: string = "All";
+  private subscriptions: Subscription;
+  protected searchBar: FormControl;
+
+  constructor(private themeService: ThemeService) { }
 
   ngOnInit() {
+
+    this.searchBar = new FormControl();
+    this.subscriptions = new Subscription();
+    this.subscriptions.add(this.themeService.darkThemeOn$.subscribe(on => this.darkThemeOn = on));
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  protected selectCategory(category: string): void {
+    this.category = category;
+  }
+
+  protected clearSearch(): void {
+    this.searchBar.setValue("");
   }
 
 }
